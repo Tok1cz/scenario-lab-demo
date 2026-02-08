cc550d955a37911d7d3ce9a66c9b2c5fbfc0d924
Konstantin Todte <konstantin.todte@tum.de>
2026-02-08 07:14:12 +0100

    feat: wrap App with SimulationProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import { theme } from "./lib/theme/antTheme";
+import { SimulationProvider } from "./hooks/useSimulation";
 import LandingPage from "./pages/LandingPage";
 import CustomizerPage from "./pages/CustomizerPage";
 import LoadingPage from "./pages/LoadingPage";
@@ -9,15 +10,17 @@ import ResultsPage from "./pages/ResultsPage";
 function App() {
   return (
     <ConfigProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/customize/*" element={<CustomizerPage />} />
-          <Route path="/simulate" element={<LoadingPage />} />
-          <Route path="/results" element={<ResultsPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <SimulationProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/customize/*" element={<CustomizerPage />} />
+            <Route path="/simulate" element={<LoadingPage />} />
+            <Route path="/results" element={<ResultsPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </SimulationProvider>
     </ConfigProvider>
   );
 }
